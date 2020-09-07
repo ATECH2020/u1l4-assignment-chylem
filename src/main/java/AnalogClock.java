d46f8f78b50211cffe3113871721feb73aa70776
Chyle Josef Masilang <chylejo.1476715@nv.ccsd.net>
2020-09-07 05:00:53 +0000

    Completed AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,11 +7,45 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int userDegrees = scanner.nextInt();
 
         /*
          *  your code goes here
          */
+        int degrees;
+        int minDegrees;
+        int min;
+
+          degrees = userDegrees - 180;
+          
+          if (degrees <=0)
+          {
+            degrees = 0;
+          }
+          else if (degrees <= 5)
+          {
+            degrees = 10;
+          }
+          else if (degrees <= 10)
+          {
+            degrees = 20;
+          }
+          else if (degrees <= 15)
+          {
+            degrees = 30;
+          }
+          else if (degrees <= 20)
+          {
+            degrees = 40;
+          }
+          else if (degrees <= 30)
+          {
+            degrees = 50;
+          }
+
+          min = degrees * 6;
+
+         System.out.println(min);
 
         // closing the scanner object
         scanner.close();
